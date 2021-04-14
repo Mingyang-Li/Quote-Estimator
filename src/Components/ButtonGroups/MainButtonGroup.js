6fd3f8bf2c7ed18a954ce4568d908b18e79907c1
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2021-04-14 18:23:24 +1200

    Restructured logics for main button group

--- src/Components/ButtonGroups/MainButtonGroup.js
+++ src/Components/ButtonGroups/MainButtonGroup.js
@@ -2,18 +2,30 @@ import Grid from '@material-ui/core/Grid';
 import Paper from "@material-ui/core/Paper"
 import NextQuestionButton from "../Buttons/MainButtons/NextQuestionButton";
 import PreviousQuestionButton from "../Buttons/MainButtons/PreviousQuestionButton";
+import { useContext } from "react";
+import GlobalContext from "../../Contexts/GlobalContext";
+import QuestionData from "../../Components/QuestionPageContents/QuestionData";
 
 const MainButtonGroup = () => {
+  const { questionIndex } = useContext(GlobalContext);
   return (
     <>
-      <Grid container spacing={1} direction-lg-row-reverse>
-        <Grid item xs={6} sm={12} lg={12}>
-          <PreviousQuestionButton/>
-        </Grid>
-        <Grid item xs={6} sm={12} lg={12}> 
-          <NextQuestionButton/>
-        </Grid>
+    <Grid container spacing={1} direction-lg-row-reverse>
+      <Grid item xs={6} sm={12} lg={12}>
+        <PreviousQuestionButton/>
       </Grid>
+      {
+        questionIndex + 1 < QuestionData.length ? 
+        (
+          <Grid item xs={6} sm={12} lg={12}> 
+            <NextQuestionButton/>
+          </Grid>
+        ) : (
+          <div></div>
+        )
+      }
+      
+    </Grid>
       <br></br>
     </>
   );
